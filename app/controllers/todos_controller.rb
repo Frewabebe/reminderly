8d4b8c3a704bacd4dd21f9bed8b8361702db480b
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-04-19 11:20:29 -0400

    Add edit feature

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,6 +18,16 @@ class TodosController < ApplicationController
     redirect_to @todo
   end
 
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update(todo_params)
+    redirect_to @todo
+  end
+
   def destroy
     @todo = Todo.find(params[:id])
     @todo.destroy
