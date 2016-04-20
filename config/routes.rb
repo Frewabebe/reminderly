95c7ca296943e01e66b2eb6b78e8af4cf4573cba
Adam Bray <adam.bray@yale.edu>
2016-04-20 11:46:11 -0400

    adds route and view for mark complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "todos#index"
-  get 'incomplete' => 'todos#incomplete'
 
-  resources :todos
+  resources :todos do
+    get 'incomplete', on: :collection
+    put 'mark_complete', on: :member
+  end
 end
